301d4d82845b58aaad5ea91b962f911c2acb597e
Nicholas Le <nle031@west-mec.org>
2019-02-12 18:15:17 -0700

    Final Commit
    Added comments and added code to display the map and the date.
    Ready for review.

--- sd_mapper.js
+++ sd_mapper.js
@@ -1,3 +1,4 @@
+"use strict"
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
@@ -8,3 +9,18 @@
    Date:  02/12/19 
 
 */
+//Changes the date
+var thisTime = new Date();
+var timeStr = thisTime.toLocaleString();
+
+//Displays the date
+document.getElementById("timeStamp").innerHTML = timeStr;
+
+//Gets hours and months
+var thisHour = thisTime.getHours();
+var thisMonth = thisTime.getMonth();
+var mapNum = (2 * thisMonth + thisHour) % 24;
+
+//Shows the sky map img on the web browser
+var imgStr = "<img src = 'sd_sky" + mapNum + ".png'/>";
+document.getElementById("planisphere").insertAdjacentHTML('afterbegin', imgStr);
